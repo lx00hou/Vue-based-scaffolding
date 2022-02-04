32a0bffb8a99a8894eb56f19d463227cfa819a05
铁蛋 <1132125385@qq.com>
2022-02-04 14:52:46 +0800

    动态生成子路由

--- src/router/autoload.ts
+++ src/router/autoload.ts
@@ -10,13 +10,17 @@ import { RouteRecordRaw } from "vue-router";
 // } 
 
 // import.meta.globEager()    vite api 遍历文件方法
-const layouts = import.meta.globEager('../layouts/*.vue')
+const layouts = import.meta.globEager('../layouts/*.vue');
+// 动态生成子路由
+const views = import.meta.globEager('../views/**/*.vue');
+
 
 function getRoutes(){
     const layoutRoutes = [] as RouteRecordRaw[]
 
     Object.entries(layouts).forEach(([file,module]) => {
         const route = getRouteByModule(file,module);
+        route.children = getChildrenRoutr(route);
         layoutRoutes.push(route)
     })
     return layoutRoutes
@@ -24,13 +28,24 @@ function getRoutes(){
 
 function getRouteByModule (file:string,module:{[key:string]:any}){
     // const name = file.split('/').pop()?.split('.')[0];
-    const name = file.replace(/.+layouts\/|\.vue/gi,'');
+    const name = file.replace(/.+layouts\/|.+views\/|\.vue/gi,'');
     const route = {
-        name,
+        name:name.replace('/','.'),
         path:`/${name}`,
         component:module.default
     }
     return route
+}
+//  动态获取子路由
+function getChildrenRoutr(layoutRoute:RouteRecordRaw){
+    const routes = [] as RouteRecordRaw[];
+    Object.entries(views).forEach(([file,module]) => {
+        if(file.includes(`../views/${layoutRoute.name}`)){
+            const route = getRouteByModule(file,module);
+            routes.push(route);
+        }
+    })
+    return routes
 }
 
 export default getRoutes()
