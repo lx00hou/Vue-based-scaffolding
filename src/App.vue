ca3e18c1e94e78ab9d7ce428f7ec45fd2e72674e
铁蛋 <1132125385@qq.com>
2022-02-07 21:33:02 +0800

    环境变量问题整体优化

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import helper,{env} from '@/types/helper';
+console.log(helper.env);
+console.log(env);
 </script>
 
 <template>
--- vite/util.ts
+++ vite/util.ts
@@ -1,15 +1,12 @@
 import _ from 'lodash'
 // 针对配置的环境变量(.env文件)的value 都是字符串问题 进行解决
 export function parseEnv(env:Record<string,any>):ViteEnv{
-    const envs:any = _.cloneDeep(env)    // 通过lodash 进行数据深拷贝
+    const envs:any = _.cloneDeep(env)    
     Object.entries(env).forEach(([key,value]) => {
-        if(['true','false'].includes(value)){
-            envs[key] = value == 'true' ? true : false
-        }
-
-        if(/^\d+$/.test(value)){
-            envs[key] = parseInt(value)
-        }
+        if(['true','false'].includes(value)) envs[key] = value == 'true' ? true : false
+        else if(/^\d+$/.test(value)) envs[key] = Number(value)
+        else if(value == "null") envs[key] = null
+        else if(value == "undefined") envs[key] = undefined
     })
     return envs
 }
