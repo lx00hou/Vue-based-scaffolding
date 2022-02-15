1f2ed1eecc200d69684473717e787fd4751dfd5f
铁蛋 <1132125385@qq.com>
2022-02-15 21:55:34 +0800

    tailwindcss以及登录页轮廓

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
-import userApi from '@/apis/userApi';
-const request = await userApi.info()
-console.log(request.result.name); 
 </script>
 
 <template>
-<ul class="bg-red-800">
-  <li>首页</li>
-  <li>会员</li>
-  <li>文章</li>
-</ul>
   <router-view />
 </template>
 
---
+++ src/views/auth/login.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+<main class=" bg-slate-400 h-screen flex justify-center items-center">
+      <div class="w-[720px] bg-white  grid grid-cols-2 rounded-md shadow-md">
+         <div class="p-6">
+            <h2 class=" text-center text-gray-700 text-lg">会员登录</h2>
+            <div>
+            </div>
+         </div>
+         <div>
+            <img src="../../../public/img/login.jpg" class=" h-96 w-full object-cover" alt="">
+         </div>
+      </div>
+</main>
+</template>
+
+<style scoped>
+</style>
