aabb2cbe4958b7abde5c0b4eb8c39a694d2ee51d
铁蛋 <1132125385@qq.com>
2022-04-20 22:03:05 +0800

    站内消息组件

---
+++ src/composables/notification.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { ref,nextTick } from "vue";
+
+const activeName = ref('site');
+const show = ref(false);
+nextTick(() => {
+    document.documentElement.addEventListener('click',() => {
+        show.value = false
+    })
+})
+</script>
+
+<template>
+<main class="notification">
+    <i class="fas fa-align-justify" @click.stop="show = !show" />
+    <el-tabs v-model="activeName" class="lists" v-show="show" @click.stop>
+        <el-tab-pane label="系统通知" name="site" >
+            <a href="" >
+                测试文字文字测试文字文字测试文字文字测试文字文字测试文字文字测试文字文字测试文字文字测试``
+            </a>
+            <a href="" >
+                测试文字文字测试文字文字测试文字文字测试文字文字测试文字文
+            </a>
+            <a href="" >
+                测试文字文字测试文字文字测试文字文字测试文字文字
+            </a>
+        </el-tab-pane>
+        <el-tab-pane label="站内消息" name="message">Name</el-tab-pane>
+    </el-tabs>
+</main>
+</template>
+
+
+<style lang="scss">
+.notification{
+    @apply relative;
+    .lists{
+        @apply absolute w-[300px] right-[-60px] bg-white px-3 py-2 border rounded-md shadow-md z-10 text-sm;
+        a{
+            @apply truncate overflow-hidden block border-b pb-2 my-2 hover:text-fuchsia-400;
+        }
+    }
+}
+</style>
--- src/layouts/admin/navbar.vue
+++ src/layouts/admin/navbar.vue
@@ -2,6 +2,7 @@
 import user from '@/store/userStore';
 import utils from '@/utils'; 
 import menuService from '@/composables/menu'
+import Notification from '@/composables/notification.vue'
 
 const userStroe = user();
 const fullScreen = () => {
@@ -21,26 +22,32 @@ const fullScreen = () => {
       <el-breadcrumb-item>富文本编辑器</el-breadcrumb-item>
     </el-breadcrumb>
   </div>
-  <div class="flex justify-center items-center relative group cursor-pointer">
+  <div class="flex justify-center items-center relative cursor-pointer">
+      <Notification class="mr-8" />
       <i class="fas fa-down-left-and-up-right-to-center mr-5" @click="fullScreen" />
-      <img :src="userStroe.info?.avatar"  class=" w-8 h-8 rounded-full object-cover">
-      <span class="ml-2 text-sm text-gray-600">
-        {{userStroe.info?.name}}
-      </span>
-      <section class="group-hover:block absolute top-full bg-white shadow-sm px-5 whitespace-nowrap border rounded-md hidden">
-        <div class="flex items-center cursor-pointer border-b py-3">
-          <a class="far fa-folder-open"></a>
-          <span class="text-xs text-gray-600 ml-2">文档资料</span>
+      <div class="group relative">
+        <div class="flex justify-center items-center">
+          <img :src="userStroe.info?.avatar"  class=" w-8 h-8 rounded-full object-cover">
+          <span class="ml-2 text-sm text-gray-600">
+            {{userStroe.info?.name}}
+          </span>
         </div>
-        <div class="flex items-center cursor-pointer border-b py-3">
-          <a class="fas fa-home"></a>
-          <span class="text-xs text-gray-600 ml-2">网站首页</span>
-        </div>
-        <div class="flex items-center cursor-pointer border-b py-3" @click="utils.user.logout()">
-          <a class="fas fa-sign-out-alt"></a>
-          <span class="text-xs text-gray-600 ml-2">退出登录</span>
-        </div>
-      </section>
+        <section class="group-hover:block absolute right-0 top-full z-50 bg-white shadow-sm px-5 whitespace-nowrap border rounded-md hidden">
+          <div class="flex items-center cursor-pointer border-b py-3">
+            <a class="far fa-folder-open"></a>
+            <span class="text-xs text-gray-600 ml-2">文档资料</span>
+          </div>
+          <div class="flex items-center cursor-pointer border-b py-3">
+            <a class="fas fa-home"></a>
+            <span class="text-xs text-gray-600 ml-2">网站首页</span>
+          </div>
+          <div class="flex items-center cursor-pointer border-b py-3" @click="utils.user.logout()">
+            <a class="fas fa-sign-out-alt"></a>
+            <span class="text-xs text-gray-600 ml-2">退出登录</span>
+          </div>
+        </section>
+      </div>
+      
   </div>
 </main>
 </template>
